e8ba769e85b37dc3d324739ff7d2cccc352307e2
IrriionDeveloper <168735493+IrriionDeveloper@users.noreply.github.com>
2024-06-03 05:58:48 -0700

    03-06-24

--- resources/views/superadmin/members/index.blade.php
+++ resources/views/superadmin/members/index.blade.php
@@ -27,7 +27,7 @@
                         <table id="ledger-datatable" class="display" style="width:100%">
                             <thead>
                                 <tr>
-                                    <th>SR. NO.</th>
+                                    {{-- <th>SR. NO.</th> --}}
                                     <th>Name</th>
                                     <th>Alias</th>
                                     <th>Parent</th>
@@ -40,120 +40,128 @@
                             <tbody>
                                 <!-- Data will be populated by DataTables -->
                             </tbody>
+                            <tfoot>
+                                <tr>
+                                    <th colspan="4">Total</th>
+                                    <th></th>
+                                    <th></th>
+                                    <th></th>
+                                </tr>
+                            </tfoot>
                         </table>
                     </div>
                 </div>
             </div>
             @push('javascript')
                 <script>
-                  $(document).ready(function() {
-    var table = $('#ledger-datatable').DataTable({
-        processing: true,
-        serverSide: true,
-        ajax: {
-            url: "{{ route('members.get-data') }}",
-            type: 'GET',
-            data: function(d) {
-                d.guid = "{{ $societyGuid }}";
-                d.group = "{{ $group }}"; // Pass the group parameter
-            }
-        },
-        columns: [
-            {data: 'id', name: 'id'},
-            {
-                data: 'name',
-                name: 'name',
-                render: function(data, type, row, meta) {
-                    var url = "{{ route('vouchers.index') }}?ledger_guid=" + row.guid;
-                    return '<a href="' + url + '" style="color: #337ab7;">' + data + '</a>';
-                }
-            },
-            {data: 'alias1', name: 'alias1'},
-            {data: 'parent', name: 'parent'},
-            {data: 'primary_group', name: 'primary_group'},
-            {data: 'this_year_balance', name: 'this_year_balance'},
-            {data: 'vouchers_count', name: 'vouchers_count'},
-            {
-                data: 'first_voucher_date',
-                name: 'first_voucher_date',
-                render: function(data, type, row, meta) {
-                    return new Date(data).toISOString();
-                }
-            }
-        ],
-        dom: 'Blfrtip',
-        lengthMenu: false, // Disable length menu
-        buttons: [
-            {
-                extend: 'excel',
-                exportOptions: {
-                    columns: ':visible',
-                    modifier: {
-                        page: 'all' // Export all pages of data
-                    }
-                }
-            },
-            {
-                extend: 'pdf',
-                exportOptions: {
-                    columns: ':visible',
-                    modifier: {
-                        page: 'all' // Export all pages of data
-                    }
-                }
-            },
-            {
-                extend: 'print',
-                exportOptions: {
-                    columns: ':visible',
-                    modifier: {
-                        page: 'all' // Export all pages of data
-                    }
-                }
-            },
-            'colvis',
-            {
-                extend: 'searchBuilder',
-                config: {
-                    columns: [0, 1, 2, 3, 4, 5, 6, 7]
-                },
-                i18n: {
-                    conditions: {
-                        date: {
-                            '=': 'Equals',
-                            '!=': 'Not equal',
-                            'before': 'Before',
-                            'after': 'After'
-                        }
-                    },
-                    date: {
-                        format: 'YYYY-MM-DD'
-                    }
-                }
-            }
-        ],
-        paging: false, // Disable pagination
-        order: [[0, 'asc']],
-        footerCallback: function (row, data, start, end, display) {
-            var api = this.api();
-            var balanceTotal = api.column(5, { page: 'current' }).data().reduce(function (acc, val) {
-                return acc + parseFloat(val);
-            }, 0);
-            var vouchersTotal = api.column(6, { page: 'current' }).data().reduce(function (acc, val) {
-                return acc + parseFloat(val);
-            }, 0);
+       $(document).ready(function() {
+                        var table = $('#ledger-datatable').DataTable({
+                            processing: true,
+                            serverSide: true,
+                            ajax: {
+                                url: "{{ route('members.get-data') }}",
+                                type: 'GET',
+                                data: function(d) {
+                                    d.guid = "{{ $societyGuid }}";
+                                    d.group = "{{ $group }}"; // Pass the group parameter
+                                }
+                            },
+                            columns: [
+                                // {data: 'id', name: 'id'},
+                                {
+                                    data: 'name',
+                                    name: 'name',
+                                    render: function(data, type, row, meta) {
+                                        var url = "{{ route('vouchers.index') }}?ledger_guid=" + row.guid;
+                                        return '<a href="' + url + '" style="color: #337ab7;">' + data + '</a>';
+                                    }
+                                },
+                                {data: 'alias1', name: 'alias1'},
+                                {data: 'parent', name: 'parent'},
+                                {data: 'primary_group', name: 'primary_group'},
+                                {data: 'this_year_balance', name: 'this_year_balance'},
+                                {data: 'vouchers_count', name: 'vouchers_count'},
+                                {
+                                    data: 'first_voucher_date',
+                                    name: 'first_voucher_date',
+                                    render: function(data, type, row, meta) {
+                                        return new Date(data).toISOString();
+                                    }
+                                }
+                            ],
+                            dom: 'Blfrtip',
+                            lengthMenu: false, // Disable length menu
+                            buttons: [
+                                {
+                                    extend: 'excel',
+                                    exportOptions: {
+                                        columns: ':visible',
+                                        modifier: {
+                                            page: 'all' // Export all pages of data
+                                        }
+                                    }
+                                },
+                                {
+                                    extend: 'pdf',
+                                    exportOptions: {
+                                        columns: ':visible',
+                                        modifier: {
+                                            page: 'all' // Export all pages of data
+                                        }
+                                    }
+                                },
+                                {
+                                    extend: 'print',
+                                    exportOptions: {
+                                        columns: ':visible',
+                                        modifier: {
+                                            page: 'all' // Export all pages of data
+                                        }
+                                    }
+                                },
+                                'colvis',
+                                {
+                                    extend: 'searchBuilder',
+                                    config: {
+                                        columns: [0, 1, 2, 3, 4, 5, 6, 7]
+                                    },
+                                    i18n: {
+                                        conditions: {
+                                            date: {
+                                                '=': 'Equals',
+                                                '!=': 'Not equal',
+                                                'before': 'Before',
+                                                'after': 'After'
+                                            }
+                                        },
+                                        date: {
+                                            format: 'YYYY-MM-DD'
+                                        }
+                                    }
+                                }
+                            ],
+                            paging: false, // Disable pagination
+                            order: [[0, 'asc']],
+                            footerCallback: function (row, data, start, end, display) {
+                                var api = this.api();
+                                var balanceTotal = api.column(4, { page: 'current' }).data().reduce(function (acc, val) {
+                                    return acc + parseFloat(val);
+                                }, 0);
+                                var vouchersTotal = api.column(5, { page: 'current' }).data().reduce(function (acc, val) {
+                                    return acc + parseFloat(val);
+                                }, 0);
 
-            $(api.column(5).footer()).html('Total Balance: ' + balanceTotal.toFixed(2));
-            $(api.column(6).footer()).html('Total Vouchers: ' + vouchersTotal.toFixed(2));
-        }
-    });
+                                $(api.column(4).footer()).html(balanceTotal.toFixed(2));
+                                $(api.column(5).footer()).html(vouchersTotal.toFixed(2));
+                            }
 
-    $('#clear-filters').click(function () {
-        $("#name").val('').trigger('change');
-        table.search('').draw();
-    });
-});
+                        });
 
+                        $('#clear-filters').click(function () {
+                            $("#name").val('').trigger('change');
+                            table.search('').draw();
+                        });
+                    });
 
 
                 </script>
